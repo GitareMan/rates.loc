d1bc269be0c0da704bf414f68cfa3c29c10884aa
admin <user@uker.net>
2018-01-30 13:28:51 +0200

    db.php

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=rates_db', //здесь изменить на свои настройки
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
